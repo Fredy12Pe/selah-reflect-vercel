b8b719b644145b3c976ed74c1b8450c3ea0a56cf
Fredy Pedro <38547323+Fredy12Pe@users.noreply.github.com>
2025-04-26 18:21:33 -0700

    Simplify Next.js configuration: minimal .babelrc, simplified next.config.js, add empty pages directory

--- next.config.js
+++ next.config.js
@@ -1,24 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-  images: {
-    domains: ['images.unsplash.com'],
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '**.googleapis.com',
-      },
-      {
-        protocol: 'https',
-        hostname: '**.firebaseapp.com',
-      },
-    ],
-  },
-  // Enable Vercel's instrumentation for performance monitoring
-  experimental: {
-    instrumentationHook: true,
-  }
+  swcMinify: true
 };
 
 module.exports = nextConfig; 
